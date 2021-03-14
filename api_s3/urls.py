bcfe2873dfd5b3995d93bb7d476498533883b75b
SuchonC <suchon_foke@windowslive.com>
2021-03-14 20:32:33 +0700

    Change route format

--- api_s3/urls.py
+++ api_s3/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('user/register', views.register, name='register'),
+    path('user/login', views.login, name="login"),
+    path('user/update-password', views.updatePassword, name="updatePassword")
 ]
--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('api_s3/', include('api_s3.urls')),
+    path('', include('api_s3.urls')),
     path('admin/', admin.site.urls),
 ]
