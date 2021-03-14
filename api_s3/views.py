85a8b5a48c2352ce5ab45f7a04e2fdad9133bda9
SuchonC <suchon_foke@windowslive.com>
2021-03-14 15:18:06 +0700

    Supports 'login' with dummy hash

--- api_s3/views.py
+++ api_s3/views.py
@@ -18,17 +18,37 @@ s3 = boto3.client(
 
 BUCKET_NAME = "s3warmup"
 
+def hash(content):
+    return content
+
 def handleNewUser(request):
     username = request.GET.get("username", None)
     password = request.GET.get("password", None)
-    if not username or not password :
+    if not username or not password:
         return HttpResponse("Username and password are needed")
 
-    hashed_password = password.encode()
+    hashed_password = hash(password).encode()
     s3.put_object(Body=hashed_password, Key=username, Bucket=BUCKET_NAME)
     return HttpResponse("Done")
+
+def handleLogin(request):
+    username = request.GET.get("username", None)
+    password = request.GET.get("password", None)
+    if not username or not password:
+        return HttpResponse("Username and password are needed")
+    
+    byte_hashed_password = None
+    try:
+        byte_hashed_password = s3.get_object(Bucket=BUCKET_NAME, Key=username)['Body'].read()
+    except s3.exceptions.NoSuchKey:
+        return HttpResponse("Username not found")
+
+    if hash(password).encode() == byte_hashed_password:
+        return HttpResponse("Done")
+    return HttpResponse("Password is invalid")
 
 def index(request):
     command = request.GET.get('command', None)
     if not command : return HttpResponse("Please enter a command")
     if command == 'newuser': return handleNewUser(request)
+    elif command == 'login': return handleLogin(request)
